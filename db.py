6c3e16021252623e0995a6c78c2191efcdbd21dc
meijiishinislame <meijiishinislame@gmail.com>
2020-02-25 22:25:46 -1000

    added time randomness

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import os
+import random
 import psycopg2
 from datetime import datetime
 from pytz import timezone
@@ -17,8 +18,6 @@ def create_ssl_certs():
 	with open(ssl_root_cert_path, 'w+') as f:
 		f.write(os.environ["SSL_ROOT_CERT"])
 
-
-
 def get_prompts():
 	conn = None
 	prompts_unweighted = []
@@ -26,6 +25,12 @@ def get_prompts():
 
 	try:
 		file = open(ssl_cert_path, 'r')
+		file.close()
+
+		file = open(ssl_key_path, 'r')
+		file.close()
+
+		file = open(ssl_root_cert_path, 'r')
 		file.close()
 	except:
 		create_ssl_certs()
@@ -59,10 +64,48 @@ def get_prompts():
 			conn.close()
 		return prompts_unweighted, prompts_weighted
 
+def get_first_date():
+	conn = None
+	try:
+		conn = psycopg2.connect(database=str(os.environ["DB_NAME"]),
+							user=str(os.environ["DB_USERNAME"]),
+							password=str(os.environ["DB_PASSWORD"]),
+							host=str(os.environ["DB_HOSTNAME"]),
+							port=str(os.environ["DB_PORT"]),
+							sslcert=ssl_cert_path,
+							sslmode=str(os.environ["SSL_MODE"]),
+							sslrootcert=ssl_root_cert_path,
+							sslkey=ssl_key_path)
+		cur = conn.cursor()
+		cur.execute("SELECT insertdate FROM arrivals LIMIT 1")
+
+		first_date = cur.fetchone()
+		cur.close()
+
+	except (Exception, psycopg2.DatabaseError) as error:
+		print(error)
+	finally:
+		if conn is not None:
+			conn.close()
+		return first_date
+
+
+def get_random_date():
+	date_format = "%m-%d-%Y"
+	first_date = get_first_date().strftime(date_format)
+	yesterday = datetime.now(timezone('US/Hawaii')).date() - timedelta(days=1)
+	yesterday = yesterday.strftime(date_format)
+	prop =  random.random()
+
+	ptime = first_date + prop * (yesterday - first_date)
+	return ptime
+
+def build_tweet_from_weighted_list(prompts_unweighted, prompts_weighted):
+	prompt = random.choice(prompts_weighted)
+
+	if prompt == prompts_unweighted[0]:
+		pass
+
 prompts_unweighted, prompts_weighted = get_prompts()
 
-print(prompts_unweighted)
-
-print("\n\n\n\n\n\n\n")
-
-print(prompts_weighted)
+print(get_random_date)
