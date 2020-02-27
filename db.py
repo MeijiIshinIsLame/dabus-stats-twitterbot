6ec10e13e98ced06f894b39eab5384eb84dfe363
meijiishinislame <meijiishinislame@gmail.com>
2020-02-27 11:38:58 -1000

    testing

--- db.py
+++ db.py
@@ -131,7 +131,7 @@ def execute_sql_fetchone(query):
 
 def count_all_entries():
 	results = execute_sql_fetchone("SELECT COUNT(*) FROM arrivals")
-	result[0] = count
+	count = result[0]
 	return count
 
 def get_random_stop(result_list):
